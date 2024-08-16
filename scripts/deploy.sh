d484d82fd89f773e4592e22019e93538f0ead9aa
Anton <tier8@yandex.ru>
2024-08-16 20:59:37 +0300

    Prepare for deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -19,7 +19,7 @@ git commit -m 'deploy'
 # если вы деплоите по адресу https://TieR89.github.io
 # git push -f git@github.com:TieR89/TieR89.github.io.git main
 
-# если вы деплоите по адресу https://TieR89.github.io/appBlogs
-git push -f git@github.com:TieR89/appBlogs.git master:gh-pages
+# если вы деплоите по адресу https://TieR89.github.io/appblogs
+git push -f git@github.com:TieR89/appblogs.git master:gh-pages
 
 cd -
