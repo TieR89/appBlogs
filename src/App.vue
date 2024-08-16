70c4b2b8f13b18c897e65fccba5a28626aa459cb
Anton <tier8@yandex.ru>
2024-08-16 17:42:19 +0300

    initial project setup with Vue Router + Axios

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>Vue 3 Blog</h1>
+    </header>
+    <main>
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
