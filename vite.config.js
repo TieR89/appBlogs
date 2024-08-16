4a3421bf2ac2de1cc9dbcc3de92f7e2c5b7cca59
Anton <tier8@yandex.ru>
2024-08-16 20:50:28 +0300

    Prepare for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from 'node:url'
-
-// Определение базового пути на основе переменной окружения
-const isProduction = process.env.NODE_ENV === 'production'
 
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  base: isProduction ? '/appBlogs/' : '/'
+  base: '/appBlogs/' // Укажите имя вашего репозитория
 })
