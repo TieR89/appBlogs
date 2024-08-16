a1db257619ad57ed6dd9ae9987636b56ddb9125d
Anton <tier8@yandex.ru>
2024-08-16 19:06:54 +0300

    Prepare for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/appBlogs/' : '/'
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  base: import.meta.env.MODE === 'production' ? '/appBlogs/' : '/'
 })
